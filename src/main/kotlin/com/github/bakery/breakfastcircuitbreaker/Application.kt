dacf89882edea9813923c08e2a4263c5357ea24a
hotire <gngh0101@gmail.com>
2022-10-09 15:54:44 +0900

    Simplify

--- src/main/kotlin/com/github/bakery/breakfastcircuitbreaker/Application.kt
+++ src/main/kotlin/com/github/bakery/breakfastcircuitbreaker/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BreakfastCircuitBreakerApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<BreakfastCircuitBreakerApplication>(*args)
+    runApplication<Application>(*args)
 }
