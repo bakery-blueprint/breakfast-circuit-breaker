0a67983af0d6e79f94e83f898716251586a6d312
hotire <gngh0101@gmail.com>
2022-10-17 01:08:32 +0900

    Verify post

--- src/test/kotlin/com/github/bakery/breakfastcircuitbreaker/controller/CircuitBreakerEndpointTest.kt
+++ src/test/kotlin/com/github/bakery/breakfastcircuitbreaker/controller/CircuitBreakerEndpointTest.kt
@@ -3,10 +3,15 @@ package com.github.bakery.breakfastcircuitbreaker.controller
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.TestConstructor
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 
+/**
+ * @see io.github.resilience4j.circuitbreaker.monitoring.endpoint.CircuitBreakerEndpoint
+ */
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
@@ -17,5 +22,13 @@ class CircuitBreakerEndpointTest(private val mockMvc: MockMvc) {
         mockMvc.get("/actuator/circuitbreakers")
             .andDo { print() }
             .andExpect { status { isOk() } }
+    }
+
+    @Test
+    fun post() {
+        mockMvc.post("/actuator/circuitbreakers/loadUser") {
+            content = """{ "updateState" : "FORCE_OPEN"}"""
+            contentType = MediaType.APPLICATION_JSON
+        }.andDo { print() }.andExpect { status { isOk() } }
     }
 }
